1f0d96a03fa9e067923f7939988e39b3a5bb00d7
pr0chy <pystole@gmail.com>
2017-03-08 11:47:40 +0100

    Nicer, shorter and faster scala version

--- snake_cube.scala
+++ snake_cube.scala
@@ -1,127 +1,85 @@
-import scala.collection.immutable.HashMap
 
-case class Point(val x: Int, val y: Int, val z: Int) {
-  private def byElement(p:Point, f:(Int, Int) => Int) = new Point(f(x, p.x), f(y, p.y), f(z, p.z))
-  def +(p:Point) = byElement(p, _ + _)
-  def -(p:Point) = byElement(p, _ - _)
-  def min(p:Point) = byElement(p, math.min)
-  def max(p:Point) = byElement(p, math.max)
-  def toList = List(x, y, z)
+case class Point(x:Int, y:Int, z:Int) {
+  def +(p:Direction):Point = Point(x + p.x, y + p.y, z + p.z)
 }
 
-object Point {
-  val zero = Point(0, 0, 0)
-  val dirRight = Point(1, 0, 0)
-  val dirDown = Point(0, 1, 0)
+sealed abstract class Direction(val x:Int, val y:Int, val z:Int) {
+  def normals:Set[Direction] =
+    this match {
+      case Up | Down => Set(Right, Left, Forward, Back)
+      case Right | Left => Set(Up, Down, Forward, Back)
+      case Forward | Back => Set(Right, Left, Up, Down)
+    }
 }
 
-val norm = List((0, 1), (1, 0), (0, -1), (-1, 0))
-val xnorm = for (n <- norm) yield Point(0, n._1, n._2)
-val ynorm = for (n <- norm) yield Point(n._1, 0, n._2)
-val znorm = for (n <- norm) yield Point(n._1, n._2, 0)
+case object Up extends Direction(0, 0, -1)
+case object Down extends Direction(0, 0, 1)
+case object Right extends Direction(0, 1, 0)
+case object Left extends Direction(0, -1, 0)
+case object Forward extends Direction(1, 0, 0)
+case object Back extends Direction(-1, 0, 0)
 
-def normals(vector: Point): List[Point] = vector match {
-  case Point(x, 0, 0) if x != 0 => xnorm
-  case Point(0, y, 0) if y != 0 => ynorm
-  case Point(0, 0, z) if z != 0 => znorm
-  case _ => throw new IllegalArgumentException("Invalid direction vector " + vector)
+case class Directions(right:Direction = Right, down:Direction = Down) {
+  def rotateAroundRight:Set[Directions] = for (down <- right.normals) yield Directions(right, down)
+  def rotateAroundDown:Set[Directions] = for (right <- down.normals) yield Directions(right, down)
 }
 
-type Result = Map[Point, Int]
+type Input = List[Char]
+type Path = List[Point]
+type Result = Seq[Int]
 
-class State private (
-    val input: List[Char],
-    private val index: Int = 0,
-    private val pos: Point = Point.zero,
-    private val rdir: Point = Point.dirRight,
-    private val ddir: Point = Point.dirDown,
-    private val minPos:Point = Point.zero,
-    private val maxPos:Point = Point.zero,
-    private val pset:Result = HashMap(Point.zero -> 0)) {
+def isSolution(set:Set[Point]) = set.size == 27
 
-  private def next(newPos:Point, rdir:Point, ddir:Point):Option[State] = {
-    val newMinPos = minPos.min(newPos)
-    val newMaxPos = maxPos.max(newPos)
-    val maxDim = (newMaxPos - newMinPos).toList.map(math.abs).max
-    val isValid = !pset.contains(newPos) && maxDim < 3
+def isInRange(p:Point) = {
+  val range = 0 to 2
+  (range contains p.x) && (range contains p.y) && (range contains p.z)
+}
 
-    val newIndex = index + 1
-    val newPset = pset + (newPos -> newIndex)
+def traverse(input:Input, ds:Directions, path:Path, set:Set[Point]):Set[Path] = {
+  val currentPos = path.head
+  if (!isInRange(currentPos) || (set contains currentPos)) Set()
+  else {
+    val nextSet = set + currentPos
 
-    Option(isValid).collect {
-      case true => new State(input.tail, newIndex, newPos, rdir, ddir, newMinPos, newMaxPos, newPset)
+    def move(moveDir: Direction, directions: Set[Directions]) = {
+      for {
+        dir <- directions
+        solution <- traverse(input.tail, dir, (currentPos + moveDir) :: path, nextSet)
+      } yield solution
+    }
+
+    input match {
+      case Nil => if (isSolution(nextSet)) Set(path) else Set()
+      case 'R' :: 'R' :: _ => move(ds.right, Set(ds))
+      case 'D' :: 'D' :: _ => move(ds.down, Set(ds))
+      case 'R' :: _ => move(ds.right, ds.rotateAroundRight)
+      case 'D' :: _ => move(ds.down, ds.rotateAroundDown)
+      case otherChar => throw new Exception(s"Uknown characted in input $otherChar")
     }
   }
-
-  def goRight():Iterable[State] = for (n <- next(pos + rdir, rdir, ddir)) yield n
-
-  def goDown():Iterable[State] = for (n <- next(pos + ddir, rdir, ddir)) yield n
-
-  def goRightAndRotate():Iterable[State] =
-    for (
-      d <- normals(rdir);
-      n <- next(pos + rdir, rdir, d)
-    ) yield n
-
-  def goDownAndRotate():Iterable[State] =
-    for (
-      d <- normals(ddir);
-      n <- next(pos + ddir, d, ddir)
-    ) yield n
-
-  def result:Result = {
-    // normalize result to 0..2 coordinates range
-    def coordMap(f:Point => Int) = (f(minPos) to f(maxPos)).zipWithIndex.toMap
-    val (xs, ys, zs) = (coordMap(_.x), coordMap(_.y), coordMap(_.z))
-
-    for ((p, i) <- pset) yield (Point(xs(p.x), ys(p.y), zs(p.z)) -> i)
-  }
-
-  def endOfInput:Boolean = input.isEmpty
-
 }
 
-object State {
-  def apply(input:List[Char]):State = new State(input)
+def toResult(solution:Path):Result = {
+  val coords = 0 until 3
+  for {
+    x <- coords
+    y <- coords
+    z <- coords
+  } yield solution.reverse.indexOf(Point(x, y, z))
 }
 
-def symmetric(res: Result):Boolean = {
-  val basePoints = Set(Point(0, 0, 0), Point(1, 0, 0), Point(1, 1, 0), Point(1, 1, 1))
-  val firstPoint = (for ((p, 0) <- res) yield p).head
-  basePoints.contains(firstPoint)
+def format(result:Result):String = {
+  val string = result
+    .map(index => f"$index%3d")
+    .grouped(9)
+    .map(level => level.grouped(3).map(_.mkString).mkString("\n"))
+    .mkString("\n\n")
+  s"Found result:\n\n$string\n"
 }
 
-def traverse(state:State):Iterable[Result] = {
-  if (state.endOfInput) return List(state.result).filter(symmetric)
+// Main
 
-  val nextStates = state.input match {
-    case 'R' :: 'R' :: _ => state.goRight
-    case 'D' :: 'D' :: _ => state.goDown
-    case 'R' :: _ => state.goRightAndRotate
-    case 'D' :: _ => state.goDownAndRotate
-    case _ => throw new IllegalArgumentException("Wrong input character " + state.input)
-  }
-  nextStates.flatMap(state => traverse(state))
-}
-
-def prettyPrint(res:Result):String = {
-  val indexes = 0 until 3
-  val result = StringBuilder.newBuilder
-  result ++= "Found result:\n"
-  for (z <- indexes) {
-    for (y <- indexes) {
-      for (x <- indexes) {
-        val index = res(Point(x, y, z))
-        result ++= f"$index%3d"
-      }
-      result += '\n'
-    }
-    result += '\n'
-  }
-  result.mkString
-}
-
-val input:List[Char] = io.Source.stdin.mkString.trim.toList
+val input:Input = io.Source.stdin.mkString.trim.toList
 
 if (input.size != 26) {
   println("Error: Input string has to have exactly 26 characters.")
@@ -129,11 +87,25 @@ if (input.size != 26) {
 }
 
 val time:Long = System.currentTimeMillis()
-val results = traverse(State(input))
-      .map(prettyPrint)
-      .toList
-      .distinct
 
-println(results.mkString)
-println(f"Total ${results.length} results")
+// choose starting points and directions to remove symmetrically redundant solutions
+val completeSymmetry = Seq((Right, Down))
+val twoDirectionSymmetry = Seq((Right, Down), (Down, Right))
+val startingPoints = Vector(
+  (Point(0,0,0), completeSymmetry),
+  (Point(0,0,1), twoDirectionSymmetry),
+  (Point(0,1,1), twoDirectionSymmetry),
+  (Point(1,1,1), completeSymmetry)
+)
+
+val solutions = for {
+  (startPoint, directions) <- startingPoints
+  (right, down) <- directions
+  solution <- traverse(input, Directions(right, down), startPoint :: Nil, Set())
+} yield solution
+
+solutions.foreach(toResult _ andThen format andThen println)
+
+println(f"Total ${solutions.size} results")
 println(f"Run time ${System.currentTimeMillis()-time}ms")
+
